fc7561f72940dcb77ea14f827f259844d7c01bac
ShreyeshBoppana <93028198+ShreyeshBoppana@users.noreply.github.com>
2023-11-18 23:37:47 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -14,16 +14,60 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+regd_users.post("/login", (req, res) => {
+    const { username, password } = req.body;
+  
+    // Check if username and password are provided
+    if (!username || !password) {
+      return res.status(400).json({ message: "Username and password are required." });
+    }
+  
+    // Check if the user exists in the registered users data
+    const registeredUser = users[username];
+  
+    if (registeredUser && registeredUser.password === password) {
+      return res.status(200).json({ message: "Login successful." });
+    } else {
+      return res.status(401).json({ message: "User doesn't exist" });
+    }
+  });
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
+regd_users.put("/auth/review/:isbn", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const {reviews}=req.body;
+  // Check if the book with the given ISBN exists
+  if (books.hasOwnProperty(isbn)) {
+    books[isbn].reviews=reviews;
+
+    // Check if the book has any reviews
+    return res.status(200).json({ message: "The book of isbn " + isbn+" is updated" });
+  }
+  else
+  {
+    return res.status(401).json({ message: "Updation failed"});
+  }
+  return res.status(401).json({message: "Yet to be implemented"});
 });
+
+regd_users.delete("/auth/reviewdelete/:isbn", (req,res) => {
+    //Write your code here
+    const isbn = req.params.isbn;
+    const {reviews}=req.body;
+    // Check if the book with the given ISBN exists
+    if (books.hasOwnProperty(isbn)) {
+      books[isbn].reviews={};
+  
+      // Check if the book has any reviews
+      return res.status(200).json({ message: "The book of isbn " + isbn+" is deleted" });
+    }
+    else
+    {
+      return res.status(401).json({ message: "Deletion failed"});
+    }
+    return res.status(401).json({message: "Yet to be implemented"});
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
